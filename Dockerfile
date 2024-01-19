3e87f00c4eab44001091e06d6daaeae66c44adab
Cristover Wurangian <christwurangian@gmail.com>
2024-01-19 22:55:42 +0700

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+﻿FROM mcr.microsoft.com/dotnet/runtime:8.0 AS base
+WORKDIR /app
+
+FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
+WORKDIR /src
+COPY ["Bottok.Net.csproj", "./"]
+RUN dotnet restore "Bottok.Net.csproj"
+COPY . .
+WORKDIR "/src/"
+RUN dotnet build "Bottok.Net.csproj" -c Release -o /app/build
+
+FROM build AS publish
+RUN dotnet publish "Bottok.Net.csproj" -c Release -o /app/publish /p:UseAppHost=false
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "Bottok.Net.dll"]
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+
+string command = "write your command here";
+string result = "";
+using (System.Diagnostics.Process proc = new System.Diagnostics.Process())
+{
+    proc.StartInfo.FileName = "/bin/bash";
+    proc.StartInfo.Arguments = "-c \" " + command + " \"";
+    proc.StartInfo.UseShellExecute = false;
+    proc.StartInfo.RedirectStandardOutput = true;
+    proc.StartInfo.RedirectStandardError = true;
+    proc.Start();
+
+    result += proc.StandardOutput.ReadToEnd();
+    result += proc.StandardError.ReadToEnd();
+
+    proc.WaitForExit();
+}
+
+Console.WriteLine(result);
+Console.WriteLine("Hello, World!");
